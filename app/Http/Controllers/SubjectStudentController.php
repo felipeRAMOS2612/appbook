7a37d6420b030d37f0087bc5cd81955e9983e98b
israel <sr.israelfranco@gmail.com>
2023-10-29 16:24:47 -0300

    REFACTOR:  Creación de Modelos, Controladores y Migraciones con correcciones PascalCase

---
+++ app/Http/Controllers/SubjectStudentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubjectStudent;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubjectStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubjectStudent $subjectStudent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubjectStudent $subjectStudent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SubjectStudent $subjectStudent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubjectStudent $subjectStudent)
+    {
+        //
+    }
+}
