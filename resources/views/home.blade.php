1b6ddc4c8b78a8fd3240c8d2f79b68b0e673c44d
sebastian hernandez <sebastianhernandezman@gmail.com>
2023-11-19 01:01:09 -0300

    Guardar cambios locales en home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,25 +24,29 @@
     <!-- Contenido principal -->
     <div class="container mx-auto mt-8">
         <!-- Sección de Noticias con Carrusel -->
-        <section class="mb-8">
-            <!-- Carrusel de Imágenes -->
-            <div x-data="{ slide: 0, totalSlides: 3 }">
-                <div class="relative overflow-hidden">
-                    <div class="flex transition-transform duration-300 ease-in-out" style="transform: translateX(-${slide * 100}%)">
-                    <!-- Imágenes del carrusel -->
-                        <div class="w-full">
-                            <img src="{{ asset('images/devon-divine-Hzp-1ua8DVE-unsplash 1.png') }}" alt="Imagen 1" class="w-full h-64 object-cover">
-                        </div>
-
-                <!-- Agrega más imágenes según sea necesario -->
-                    </div>
+        <div x-data="{ currentSlide: 0, totalSlides: 4 }" class="carousel flex overflow-hidden">
+                <div x-show="currentSlide === 0" class="carousel-item transition-transform duration-300 ease-in-out transform">
+                    <!-- Contenido de la primera diapositiva -->
+                    <img src="images/foto.png" alt="bilbioteca">
                 </div>
-
-        <!-- Controles del carrusel -->
-        <div class="flex mt-2">
-            <button @click="slide = (slide - 1 + totalSlides) % totalSlides" class="mr-2 px-2 py-1 bg-blue-500 text-white">Anterior</button>
-            <button @click="slide = (slide + 1) % totalSlides" class="px-2 py-1 bg-blue-500 text-white">Siguiente</button>
-        </div>
+                <div x-show="currentSlide === 1" class="carousel-item transition-transform duration-300 ease-in-out transform">
+                    <!-- Contenido de la segunda diapositiva -->
+                    <img src="images/computacion.jpg" alt="sala de computación">
+                </div>
+                <div x-show="currentSlide === 2" class="carousel-item transition-transform duration-300 ease-in-out transform">
+                    <!-- Contenido de la segunda diapositiva -->
+                    <img src="images/liceo.jpg" alt="liceo">
+                </div>
+                <div x-show="currentSlide === 3" class="carousel-item transition-transform duration-300 ease-in-out transform">
+                    <!-- Contenido de la segunda diapositiva -->
+                    <img src="images/gimnasio.jpg" alt="gimnasio">
+                </div>
+                <!-- Agrega más diapositivas y lógica de navegación -->
+                <div class="flex justify-center mt-4">
+                    <button class="px-4 py-2 border rounded bg-white text-gray-800" @click="currentSlide = (currentSlide - 1 + totalSlides) % totalSlides">Anterior</button>
+                    <button class="px-4 py-2 border rounded bg-white text-gray-800" @click="currentSlide = (currentSlide + 1) % totalSlides">Siguiente</button>
+                </div>
+            </div>
     </div>
 
             <!-- Tres Rectángulos -->
