f7c7f54b4020bc06e85e24b9e5b6b6d9c03dab07
Felipe Ramos <feliperamos2612@gmail.com>
2023-12-05 14:18:52 -0300

    CHORE: agregar recursos a vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/register-grade.js',
+                'resources/css/register-grade.css'
             ],
             refresh: true,
         }),
